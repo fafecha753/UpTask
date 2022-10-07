8ae536898bd9c5d65a2887f364860ed66bca2664
fafecha753 <fafecha753@gmail.com>
2022-10-07 12:52:58 -0600

    Iconos-Pantalla Agregar Tarea

---
+++ app/src/main/java/com/example/uptask/activity_AgregarTarea.java
@@ -0,0 +1,14 @@
+package com.example.uptask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_AgregarTarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_tarea);
+    }
+}
